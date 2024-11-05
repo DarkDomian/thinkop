8831e614e9ed3b977420b12cc9690f9c48ee64bb
DarkDomian <darkdomian@gmail.com>
2024-11-05 08:22:06 +0300

    Add homepage and deploy scripts for GitHub Pages. Also create main form template

---
+++ src/Paginator.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { PaginationProps } from 'antd';
+import { Pagination } from 'antd';
+
+import { FaArrowRight } from "react-icons/fa";
+
+const itemRender: PaginationProps['itemRender'] = (page, type, originalElement) => {
+  if (type === 'prev') {
+    return page === 0 ? <></> : <a><FaArrowRight className='rotate-180'/></a>;
+  }
+  if (type === 'next') {
+    return page === 5 ? <></> : <a><FaArrowRight /></a>;
+  }
+  // if (type === 'page') {
+    // return 
+    // if (element === 1 || element === 4 || Math.abs(element - 1) <= 1) {
+    //   return originalElement;
+    // }
+    // return <span className="ant-pagination-item-ellipsis">...</span>; // Троеточие для скрытых страниц
+  // }
+
+  return originalElement;
+};
+
+const Paginator: React.FC = () => {
+
+    
+    return (
+        <>
+        <Pagination total={50} itemRender={itemRender} showLessItems/>
+        </>
+    )
+}
+
+export default Paginator;
